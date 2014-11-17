f2b8f3aafcb5f0bbddab6745151ab3da718dc3e7
Durk <dpkingma@gmail.com>
2014-11-17 15:42:47 +0100

    Fixed missing import

--- anglepy/models/__init__.py
+++ anglepy/models/__init__.py
@@ -1,13 +1,4 @@
 
-from anglepy.models.DBN import *
-from anglepy.models.DBN_noAT import *
-from anglepy.models.DBN_scan import *
-
-from anglepy.models.MLBN import *
-from anglepy.models.MLBN_Inverse import *
-from anglepy.models.MLBN_noAT import *
-from anglepy.models.MLBNFast import *
-
 from anglepy.models.MLP_Categorical import *
 
 from VAE_Z_X import *
